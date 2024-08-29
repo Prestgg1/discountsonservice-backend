17dab428e3a64ddd08381d255645df9ec910cac9
Prestgg1 <prestgg12@gmail.com>
2024-08-29 22:44:45 +0400

    Packagejson Updated

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => {
         connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST'),
         port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
+        database: env('DATABASE_NAME', ),
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
         ssl: env.bool('DATABASE_SSL', false) && {
